30bd672a1fd23cac84c143a3f46f4435e24e789c
Gideon Amoah <giderraamoah777@gmail.com>
2025-06-26 19:54:42 +0000

    update 2.1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello world")
-print("Me")
+import random
+import string
+def create(length=10,by_nums=True,by_letters=True,by_special_charac=True):
+    character=string.ascii_letters  #for both lower and upper case letters from A-Z
+    #  using control structure
+    if by_nums:
+        character+=string.digits #thus generating characters plus numbers 
+    if by_special_charac:
+        character+=string.punctuation #thus adding characters to special characters to create a password
+    else:
+        return "Invalid"    
+    password="".join(random.choice(character) for i in range(length))
+    return password
+
