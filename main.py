a21fa1a3bdcf8b72fed9a877dc3becf85a529dc5
Gideon Amoah <giderraamoah777@gmail.com>
2025-06-27 18:36:19 +0000

    update 3

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import random
 import string
-def create(length=10,by_nums=True,by_letters=True,by_special_charac=True):
-    character=string.ascii_letters  #for both lower and upper case letters from A-Z
-    #  using control structure
-    if by_nums:
-        character+=string.digits #thus generating characters plus numbers 
-    if by_special_charac:
-        character+=string.punctuation #thus adding characters to special characters to create a password
-    else:
-        return "Invalid"    
-    password="".join(random.choice(character) for i in range(length))
+import random
+Characters=string.ascii_letters +string.digits + string.punctuation # the ascii genetares you both random uppercase and lowercase letters and the strin.digits generate numbers wwhiles the string.punctuation generates you special characters
+
+def password_generator(length=12,by_numbers=True,by_special_charc=True): #function
+    password="".join(random.choice(Characters)for i in range(12))
     return password
+
+option=input("Do you want to generate a passowrd? (Yes/No) : ")
+
+# using control flow
+if option=="Yes":
+    password_generator()  #ivoke the fuction 
+elif option=="No":
+    print("Program ended") 
+else:
+    print("Invalid input, please enter(Yes/No)")       
+
+
+
 
--- password_genrator.py
+++ password_genrator.py
@@ -1,5 +1,5 @@
 import password_genrator
 my_password=password_genrator
 user_imput=(input("Enter your password : "))
-Final_password=my_password.create(user_imput)
+Final_password=my_password.password_genrator(user_imput)
 print(Final_password)
